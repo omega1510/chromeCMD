344fe88ce9851e0d68b9dca59450783e05f78059
Hayaan I Rizvi <20472287@fhda.edu>
2021-08-26 16:14:45 -0700

    Added install script

---
+++ install/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo cd /usr/local/bin
+sudo curl -fsSL https://raw.githubusercontent.com/omega1510/chromeCMD/HEAD/chrome -o chrome
+sudo chmod +x chrome
+echo "chromeCMD was successfully installed."
